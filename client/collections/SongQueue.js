ba86e9bf082b717cffad6cfa1e29f8e19116e9bf
Hack Reactor Student <student@hackreactor.com>
2017-04-10 17:38:51 -0700

    Implemented Song Model and Song Queue
    Test pass for both Song Model and Song Queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,30 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+  	this.on('add', function(song){
+  		if(this.at(1) === undefined) {
+  			this.playFirst();
+  		}
+  	});
+
+  	this.on('ended', function(){
+  		if (this.at(1)) {
+  			this.shift();
+  			this.playFirst();
+  		}  		
+  	});
+
+  	this.on('dequeue', function(){
+  		this.remove();
+  	});
+
+  },
+
+  playFirst: function() {
+  	if(this.at(0)){
+  		this.at(0).play();
+  	}
   }
+
 
 });
